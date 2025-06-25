3c8074b4bd16f9cf7bfa5c6c11ef5e5f0ac04836
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2025-06-25 19:58:48 +0200

    Implement SWAR parsing

--- arm9/source/nitroComposer/swar.cpp
+++ arm9/source/nitroComposer/swar.cpp
@@ -1,5 +1,76 @@
 #include "swar.h"
 
-SWAR::SWAR(const std::string &fileName) : sections(fileName) {}
+#include "../binaryReader.h"
+#include "../substream.h"
 
-SWAR::SWAR(std::unique_ptr<BinaryReadStream> &&stream) : sections(std::move(stream)) {}
+#include <nds/arm9/sassert.h>
+
+SWAR::SWAR(const std::string &fileName) : sections(fileName) {
+	Parse();
+}
+
+SWAR::SWAR(std::unique_ptr<BinaryReadStream> &&stream) : sections(std::move(stream)) {
+	Parse();
+}
+
+const SWAR::Wave &SWAR::GetWaveMetaData(unsigned int waveIndex) const {
+	return waves.at(waveIndex);
+}
+
+
+std::unique_ptr<BinaryReadStream> SWAR::GetWaveData(const Wave &waveInfo) const {
+	auto section = sections.getSectionInfo("DATA");
+	auto stream = sections.getSectionData(section);
+
+	std::uint16_t sampleCount = waveInfo.loopStart + waveInfo.loopLength;
+	size_t dataSize;
+
+	switch(waveInfo.encoding) {
+	case WaveEncoding::PCM8:
+		dataSize = sampleCount * 1;
+		break;
+	case WaveEncoding::PCM16:
+		dataSize = sampleCount * 2;
+		break;
+	case WaveEncoding::ADPCM:
+		dataSize = 2 + sampleCount / 2;
+		break;
+	default:
+		sassert(0, "Bad encoding %i", (int)waveInfo.encoding);
+	}
+
+	return std::make_unique<SubStream>(stream.release(), waveInfo.dataOffset, dataSize, true);
+}
+
+void SWAR::Parse() {
+	auto section = sections.getSectionInfo("DATA");
+	auto stream = sections.getSectionData(section);
+
+	BinaryReader reader(std::move(stream));
+	reader.skip(8 * 4);//"runtime reserved" nonsense
+	std::uint32_t waveCount = reader.readLELong();
+	waves.reserve(waveCount);
+
+	for(std::uint32_t waveIndex = 0; waveIndex < waveCount; ++waveIndex) {
+		std::uint32_t offset = reader.readLELong();
+		offset -= section->offset;
+
+		auto pos = reader.getPos();
+
+		reader.setPos(offset);
+
+		Wave wave;
+
+		wave.encoding = (WaveEncoding) reader.readByte();
+		wave.loops = reader.readByte()!=0;
+		wave.sampleRate = reader.readLEShort();
+		wave.timerLen = reader.readLEShort();
+		wave.loopStart = reader.readLEShort();
+		wave.loopLength = reader.readLEShort();
+		wave.dataOffset = reader.getPos();
+
+		waves.push_back(wave);
+
+		reader.setPos(pos);
+	}
+}
--- arm9/source/nitroComposer/swar.h
+++ arm9/source/nitroComposer/swar.h
@@ -7,8 +7,34 @@ class SWAR {
 public:
 	SWAR(const std::string &fileName);
 	SWAR(std::unique_ptr<BinaryReadStream> &&stream);
+
+	enum class WaveEncoding {
+		PCM8,
+		PCM16,
+		ADPCM,
+		Generated
+	};
+
+	struct Wave {
+		WaveEncoding encoding;
+
+		bool loops;
+		std::uint16_t loopStart;
+		std::uint16_t loopLength;
+
+		std::uint16_t sampleRate;
+		std::uint16_t timerLen;
+
+		std::uint32_t dataOffset;
+	};
+
+	const Wave &GetWaveMetaData(unsigned int waveIndex) const;
+	std::unique_ptr<BinaryReadStream> GetWaveData(const Wave &waveInfo) const;
 private:
 	SectionedFile sections;
+	void Parse();
+
+	std::vector<Wave> waves;
 };
 
 #endif
