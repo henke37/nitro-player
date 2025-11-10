3d65fb2dda9248b0a5060f606296e592ceacbf04
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2025-11-10 13:15:50 +0100

    Factor out to ipc.cpp

---
+++ arm7/source/nitrocomposer/ipc.cpp
@@ -0,0 +1,110 @@
+#include "sequencePlayer.h"
+
+#include <nds/fifocommon.h>
+#include <nds/interrupts.h>
+#include <nds/arm7/console.h>
+#include <nds/system.h>
+#include <nds/arm7/audio.h>
+
+#include "nitroComposer/ipc.h"
+
+#include <cassert>
+
+namespace NitroComposer {
+
+	const size_t fifoBuffSize = 32;
+
+	void SequencePlayer::setupFifo() {
+		fifoSetDatamsgHandler(FIFO_NITRO_COMPOSER, fifoDatagramHandler, nullptr);
+	}
+
+	void SequencePlayer::fifoDatagramHandler(int num_bytes, void *userdata) {
+		sequencePlayer.fifoDatagramHandler(num_bytes);
+	}
+
+	void SequencePlayer::fifoDatagramHandler(int num_bytes) {
+		u8 fifoBuffer[fifoBuffSize];
+		fifoGetDatamsg(FIFO_USER_01, fifoBuffSize, fifoBuffer);
+
+		auto ipc = reinterpret_cast<BaseIPC *>(fifoBuffer);
+
+		switch(ipc->command) {
+
+		case BaseIPC::CommandType::PowerOn:
+		{
+			enableSound();
+
+			setupTimer();
+		} break;
+
+		case BaseIPC::CommandType::SetVar:
+		{
+			SetVarIPC *setVarIpc = static_cast<SetVarIPC *>(ipc);
+			playingSequence.SetVar(setVarIpc->var, setVarIpc->val);
+		} break;
+
+		case BaseIPC::CommandType::GetVar:
+		{
+			GetVarIPC *getVarIpc = static_cast<GetVarIPC *>(ipc);
+			std::int16_t val = playingSequence.GetVar(getVarIpc->var);
+			bool success = fifoSendValue32(FIFO_NITRO_COMPOSER, val);
+			assert(success);
+		} break;
+
+		case BaseIPC::CommandType::SetTempo:
+		{
+			SetTempoIPC *setTempoIpc = static_cast<SetTempoIPC *>(ipc);
+			this->playingSequence.tempo = setTempoIpc->tempo;
+		} break;
+		case BaseIPC::CommandType::SetMainVolume:
+		{
+			SetMainVolumeIPC *setMainVolumeIpc = static_cast<SetMainVolumeIPC *>(ipc);
+			this->mainVolume = setMainVolumeIpc->volume;
+			REG_MASTER_VOLUME = setMainVolumeIpc->volume;
+		} break;
+
+		case BaseIPC::CommandType::LoadBank:
+		{
+			LoadBankIPC *loadBankIpc = static_cast<LoadBankIPC *>(ipc);
+			this->playingSequence.bank = loadBankIpc->bank;
+
+			consolePrintf("Loaded %d instruments\n", this->playingSequence.bank->instruments.size());
+			consoleFlush();
+		} break;
+
+		case BaseIPC::CommandType::LoadWaveArchive:
+		{
+			LoadWaveArchiveIPC *loadWaveArchiveIpc = static_cast<LoadWaveArchiveIPC *>(ipc);
+			assert(loadWaveArchiveIpc->slot < PlayingSequence::numWaveArchs);
+			this->playingSequence.waveArchs[loadWaveArchiveIpc->slot] = loadWaveArchiveIpc->archive;
+
+			if(!loadWaveArchiveIpc->archive) break;
+
+			consolePrintf("Loaded bank %d with %d waves\n", loadWaveArchiveIpc->slot, loadWaveArchiveIpc->archive->waves.size());
+			consoleFlush();
+		} break;
+
+		case BaseIPC::CommandType::PlaySequence:
+		{
+			PlayTrackIPC *playTrackIpc = static_cast<PlayTrackIPC *>(ipc);
+			allowedChannels = playTrackIpc->channelMask;
+			playingSequence.sequenceVolume = playTrackIpc->sequenceVolume;
+			playingSequence.PlaySequence(playTrackIpc->sequenceData);
+		} break;
+
+		case BaseIPC::CommandType::StopSequence:
+		{
+			playingSequence.AbortSequence();
+		} break;
+
+		case BaseIPC::CommandType::ReserveChannels:
+		{
+			ReserveChannelsIPC *reserveIpc = static_cast<ReserveChannelsIPC *>(ipc);
+			externalChannelReservations = reserveIpc->reservations;
+		}
+
+		default:
+			assert(0);
+		}
+	}
+}
--- arm7/source/nitrocomposer/sequencePlayer.cpp
+++ arm7/source/nitrocomposer/sequencePlayer.cpp
@@ -1,6 +1,5 @@
 #include "sequencePlayer.h"
 
-#include <nds/fifocommon.h>
 #include <nds/timers.h>
 #include <nds/interrupts.h>
 #include <nds/arm7/console.h>
@@ -9,8 +8,6 @@
 
 #include <cassert>
 
-#include "nitroComposer/ipc.h"
-
 namespace NitroComposer {
 
 	static const uint8_t pcmChannels[] = { 4, 5, 6, 7, 2, 0, 3, 1, 8, 9, 10, 11, 14, 12, 15, 13 };
@@ -18,8 +15,6 @@ namespace NitroComposer {
 	static const uint8_t noiseChannels[] = { 14, 15 };
 
 	SequencePlayer sequencePlayer;
-
-	const size_t fifoBuffSize = 32;
 
 	void SequencePlayer::Init() {
 		for(unsigned int var = 0; var < globalVariableCount; ++var) {
@@ -148,99 +143,6 @@ namespace NitroComposer {
 		return this->waveArchs[archiveSlot]->waves.at(waveIndex);
 	}
 
-	void SequencePlayer::setupFifo() {
-		fifoSetDatamsgHandler(FIFO_NITRO_COMPOSER, fifoDatagramHandler, nullptr);
-	}
-
-	void SequencePlayer::fifoDatagramHandler(int num_bytes, void *userdata) {
-		sequencePlayer.fifoDatagramHandler(num_bytes);
-	}
-
-	void SequencePlayer::fifoDatagramHandler(int num_bytes) {
-		u8 fifoBuffer[fifoBuffSize];
-		fifoGetDatamsg(FIFO_USER_01, fifoBuffSize, fifoBuffer);
-
-		auto ipc = reinterpret_cast<BaseIPC *>(fifoBuffer);
-
-		switch(ipc->command) {
-
-		case BaseIPC::CommandType::PowerOn:
-		{
-			enableSound();
-
-			setupTimer();
-		} break;
-
-		case BaseIPC::CommandType::SetVar:
-		{
-			SetVarIPC *setVarIpc = static_cast<SetVarIPC *>(ipc);
-			playingSequence.SetVar(setVarIpc->var, setVarIpc->val);
-		} break;
-
-		case BaseIPC::CommandType::GetVar:
-		{
-			GetVarIPC *getVarIpc = static_cast<GetVarIPC *>(ipc);
-			std::int16_t val = playingSequence.GetVar(getVarIpc->var);
-			bool success = fifoSendValue32(FIFO_NITRO_COMPOSER, val);
-			assert(success);
-		} break;
-
-		case BaseIPC::CommandType::SetTempo:
-		{
-			SetTempoIPC *setTempoIpc = static_cast<SetTempoIPC *>(ipc);
-			this->playingSequence.tempo = setTempoIpc->tempo;
-		} break;
-		case BaseIPC::CommandType::SetMainVolume:
-		{
-			SetMainVolumeIPC *setMainVolumeIpc = static_cast<SetMainVolumeIPC *>(ipc);
-			this->mainVolume = setMainVolumeIpc->volume;
-			REG_MASTER_VOLUME = setMainVolumeIpc->volume;
-		} break;
-
-		case BaseIPC::CommandType::LoadBank:
-		{
-			LoadBankIPC *loadBankIpc = static_cast<LoadBankIPC *>(ipc);
-			this->playingSequence.bank = loadBankIpc->bank;
-
-			consolePrintf("Loaded %d instruments\n", this->playingSequence.bank->instruments.size());
-			consoleFlush();
-		} break;
-
-		case BaseIPC::CommandType::LoadWaveArchive:
-		{
-			LoadWaveArchiveIPC *loadWaveArchiveIpc = static_cast<LoadWaveArchiveIPC *>(ipc);
-			assert(loadWaveArchiveIpc->slot < PlayingSequence::numWaveArchs);
-			this->playingSequence.waveArchs[loadWaveArchiveIpc->slot] = loadWaveArchiveIpc->archive;
-
-			if(!loadWaveArchiveIpc->archive) break;
-
-			consolePrintf("Loaded bank %d with %d waves\n", loadWaveArchiveIpc->slot, loadWaveArchiveIpc->archive->waves.size());
-			consoleFlush();
-		} break;
-
-		case BaseIPC::CommandType::PlaySequence:
-		{
-			PlayTrackIPC *playTrackIpc = static_cast<PlayTrackIPC *>(ipc);
-			allowedChannels = playTrackIpc->channelMask;
-			playingSequence.sequenceVolume = playTrackIpc->sequenceVolume;
-			playingSequence.PlaySequence(playTrackIpc->sequenceData);
-		} break;
-
-		case BaseIPC::CommandType::StopSequence:
-		{
-			playingSequence.AbortSequence();
-		} break;
-
-		case BaseIPC::CommandType::ReserveChannels:
-		{
-			ReserveChannelsIPC *reserveIpc = static_cast<ReserveChannelsIPC *>(ipc);
-			externalChannelReservations = reserveIpc->reservations;
-		}
-
-		default:
-			assert(0);
-		}
-	}
 
 	bool SequencePlayer::isVoiceAllowed(std::uint8_t voiceIndex) const {
 		if(!(allowedChannels & BIT(voiceIndex)) && allowedChannels) return false;
